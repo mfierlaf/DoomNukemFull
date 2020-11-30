1daf14338b7186f62aedf22fa66b19f41dcc09b5
user42 <vanduffelchristopher@gmail.com>
2020-11-30 03:26:25 +0100

    Editor check created

--- src/ed_check.c
+++ src/ed_check.c
@@ -19,7 +19,24 @@ static int	ed_check_sector_integrity(t_sector sect)
 
 static int	ed_check_if_is_line(t_sector sect)
 {
-	int	
+	int	i_vertex;
+	float	slope;
+
+	i_vertex = 0;
+	if (sect.npoints <= 3)
+		return (1);
+	slope = get_slope(new_line(
+			new_fpoint(sect[sect.npoints - 1].x, sect[sect.npoints - 1].y),
+			new_fpoint(sect[i_vertex].x, sect[i_vertex].y)));
+	while (i_vertex < sect.npoints - 1)
+	{
+		if (slope != get_slope(new_line(
+			new_fpoint(sect[i_vertex].x, sect[i_vertex].y),
+			new_fpoint(sect[i_vertex + 1].x, sect[i_vertex].y))))
+			return (0);
+		i_vertex++;
+	}
+	return (1);
 }
 
 int		ed_check_can_add(t_mlx *mlx, t_sector sect)
